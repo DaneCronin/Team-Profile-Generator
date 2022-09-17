6c2669c02e9a37bb446c961568cd63a3b5c3c831
Dane Cronin <danecronin@gmail.com>
2022-09-17 17:25:06 -0600

    added basic HTML structure to generatePage function in generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,4 +1,44 @@
 generateHTML = () => {}
 
+
+// generate html page - HTML Page layout
+const generatePage = function () {   
+    console.log("Page has been created!")
+
+
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+    
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+     
+    </head>
+
+    
+    <body>
+
+        <header>
+            <h1>Team Profile</h1>
+      
+        </header>
+
+        <main>
+            <div>
+
+            </div>
+           
+        </main>
+        
+    </body>
+    
+    </html>
+  `;
+  }
+
 // export to index
 module.exports = generateHTML; 
