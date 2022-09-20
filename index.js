c65aeb73d12caa6a55513ea334a9dd923f5fe21d
Dane Cronin <danecronin@gmail.com>
2022-09-20 11:45:18 -0600

    fixed bug causing tests to fail, updated Bootstrap and Popper CDN's

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //Link to generateHTML for page creation
-const generateHTML = require('./src/generateHTML')
+const generateHTML = require('./src/generateHTML');
 
 //Team profiles
+const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
